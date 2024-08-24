2ed3f32d754acee2a68f5b7747b076cb5a3ac2d1
Ai-codemaster <ai5445359@gmail.com>
2024-08-24 21:47:42 +0600

    complete header and banner section and some edit in navbar section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,7 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
-        screens: {
-          'xs': '480px',
-          'sm': '576px',
-          'md': '768px',
-          'lg': '1024px',
-          'xl': '1280px',
-          '2xl': '1536px',
-        }
+        
       },
     },
     plugins: [],
